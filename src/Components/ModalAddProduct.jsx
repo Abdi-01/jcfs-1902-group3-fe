b4714c3ae61275f9668dd27e41a5f57692c5d315
alifjunjunan <junjunanalif@gmail.com>
2022-03-31 13:25:00 +0700

    WOOD-20-02: fix format date

--- src/Components/ModalAddProduct.jsx
+++ src/Components/ModalAddProduct.jsx
@@ -83,7 +83,7 @@ const ModalAddProduct = (props) => {
 
         let formData = new FormData()
         let date = new Date()
-        date = `${date.toLocaleDateString().replaceAll('/', '-')} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+        date = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
         let data = {
             ...product,
             date: date
