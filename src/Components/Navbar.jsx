f6461ebc0ebdeb779f96387e7631c825314e8145
Abdi-01 <abdialghi@gmail.com>
2022-04-12 11:19:59 +0700

    Merge branch 'develop' of https://github.com/boblightning/jcfs-1902-group3-fe into boblightning-develop

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -26,20 +26,11 @@ const Navbar = () => {
             <Box bg={'white'} height='10vh' boxShadow='md' mb='8vh'>
                 <Center>
                     <HStack spacing={'50px'} margin='2vw' height='2vh' color={'white'}>
-                        <Text fontSize={'lg'} fontWeight='bold' color={'#6b3c3b'}>
-                            Ruang Keluarga
-                        </Text>
-                        <Text fontSize={'lg'} fontWeight='bold' color={'#6b3c3b'}>
-                            Kamar Mandi
-                        </Text>
-                        <Text fontSize={'lg'} fontWeight='bold' color={'#6b3c3b'}>
-                            Kamar Tidur
-                        </Text>
                         {printKategori()}
-                                    </HStack>
-                                </Center>
-                            </Box>
-                        </>
+                    </HStack>
+                </Center>
+            </Box>
+        </>
     )
 }
 
